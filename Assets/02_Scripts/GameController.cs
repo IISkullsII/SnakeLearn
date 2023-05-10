99e0e8ca1659edf23bf1fc34a4d80394b824fc79
Marc Hoffmann <m.h.96@live.de>
2023-05-10 15:37:18 +0200

    [+] Collisiondetection, Reset Gamestate

--- Assets/02_Scripts/GameController.cs
+++ Assets/02_Scripts/GameController.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace _02_Scripts
 {
@@ -6,10 +9,20 @@ namespace _02_Scripts
     {
 
         [SerializeField] private GameObject snakePlayer;
-        [SerializeField] private GameObject snakeFood;
+        private SnakeController _snakePlayerController;
+
+        [SerializeField] private GameObject snakeFoodPrefab;
 
         [SerializeField] private int roomWidthCol = 16;
         [SerializeField] private int roomHeightRow = 9;
+
+        [SerializeField] private GameObject deadUIObject;
+
+        public int RoomWidthCol => roomWidthCol;
+        public float RoomWidth => roomWidthCol * cellSize;
+        public int RoomHeightCol => roomHeightRow;
+        public float RoomHeight => roomHeightRow * cellSize;
+        
         [SerializeField] private float cellSize = 1f;
         public float CellSize => cellSize;
 
@@ -18,6 +31,100 @@ namespace _02_Scripts
     
         [SerializeField] private bool drawDebugGrid = false;
 
+        [SerializeField] private int amountFood = 1;
+        private List<GameObject> _listFood = new List<GameObject>();
+        public List<GameObject> ListFood => _listFood;
+
+        [SerializeField] private GameObject uiCanvas;
+        private TextFadeController _textFadeController;
+
+        private void Awake()
+        {
+            _textFadeController = uiCanvas.GetComponentInChildren<TextFadeController>();
+        }
+
+        private void Start()
+        {
+            _snakePlayerController = snakePlayer.GetComponent<SnakeController>();
+            InitFoodList();
+        }
+
+        public void TriggerGameOver()
+        {
+            uiCanvas.SetActive(true);
+            _textFadeController.StartFade();
+        }
+        
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.G))
+            {
+                SpawnNewFood();
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space) && _snakePlayerController.IsDead)
+            {
+                ResetGame();
+            }
+        }
+
+        public void EatFood(int listFoodIndex)
+        {
+            if (listFoodIndex < 0 && listFoodIndex >= _listFood.Count) return;
+
+            GameObject eatenFood = _listFood[listFoodIndex];
+            _listFood.RemoveAt(listFoodIndex);
+            Destroy(eatenFood);
+
+            if (_listFood.Count < amountFood)
+            {
+                SpawnNewFood();
+            }
+        }
+
+        private void InitFoodList()
+        {
+            for (int i = 0; i < amountFood; i++)
+            {
+                SpawnNewFood();
+            }
+        }
+
+        public void SpawnNewFood()
+        {
+            Vector3 newPos = GetRandomPointInRoom();
+            
+            GameObject newFood = Instantiate(snakeFoodPrefab, newPos, Quaternion.Euler(90f, 0f, 0f));
+            _listFood.Add(newFood);
+        }
+
+        private void ClearFoodList()
+        {
+            foreach (GameObject t in _listFood)
+            {
+                Destroy(t);
+            }
+
+            _listFood.Clear();
+        }
+
+        private Vector3 GetRandomPointInRoom()
+        {
+            Vector3 newPos = Vector3.zero;
+            newPos.x = Random.Range(0, roomWidthCol) * cellSize + (cellSize / 2) - (RoomWidth / 2);
+            newPos.y = Random.Range(0, roomHeightRow) * cellSize + (cellSize / 2) - (RoomHeight / 2);
+
+            return newPos;
+        }
+
+        private void ResetGame()
+        {
+            uiCanvas.SetActive(false);
+            ClearFoodList();
+            InitFoodList();
+            _snakePlayerController.ResetSnake();
+        }
+        
         private void OnDrawGizmos()
         {
             if (!this.drawDebugGrid) return;
--- Assets/02_Scripts/SnakeController.cs
+++ Assets/02_Scripts/SnakeController.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace _02_Scripts
@@ -11,10 +13,12 @@ namespace _02_Scripts
 
         [SerializeField] private float snakeSpeed = 1f;     // Factor for timeTillMove
         [SerializeField] private float timeTillMove = 1f;   // In Seconds
+        [SerializeField] private int snakeBodyStart = 3;
         
         private Vector3 _snakeDirection = Vector3.up;
 
         private bool _isDead = false;
+        public bool IsDead => _isDead;
 
         [SerializeField] private GameObject gameControllerObject;
         private GameController _gameController;
@@ -32,7 +36,7 @@ namespace _02_Scripts
     
         private void Start()
         {
-            _snakeBody = new SnakeBody(snakeBodyPart, gameObject.transform, 3);
+            _snakeBody = new SnakeBody(snakeBodyPart, gameObject.transform, snakeBodyStart);
             
             StartCoroutine(MoveSnake());
         }
@@ -46,6 +50,14 @@ namespace _02_Scripts
             {
                 _snakeBody.AddBodyPart();
             }
+        }
+
+        public void ResetSnake()
+        {
+            _snakeBody.ResetBody(snakeBodyStart);
+            transform.position = new Vector3(_gameController.CellSize / 2, _gameController.CellSize / 2, 0);
+            _isDead = false;
+            StartCoroutine(MoveSnake());
         }
 
     
@@ -80,22 +92,33 @@ namespace _02_Scripts
         {
             while (!_isDead)
             {
-                _snakeBody.MoveBodyParts(transform.position);
+                Vector3 transformPos = transform.position;
+                _snakeBody.MoveBodyParts(transformPos);
                 
                 float movementMagnitude = _gameController.CellSize;
 
-                if (CheckCollision(_snakeDirection * movementMagnitude))
+                Vector3 nextPosition = transformPos + (_snakeDirection * movementMagnitude);
+                
+                if (CheckCollision(nextPosition))
                 {
                     _isDead = true;
+                    _gameController.TriggerGameOver();
                     yield return null;
                 }
-                
+
+                int eatFood = CheckFoodCollision(nextPosition);
+                if (eatFood >= 0)
+                {
+                    _gameController.EatFood(eatFood);
+                    _snakeBody.AddBodyPart();
+                }
+
                 transform.position += _snakeDirection * movementMagnitude;
 
                 if (debugDrawDirection)
                 {   
                 
-                    Debug.DrawLine(transform.position, transform.position + (_snakeDirection * (movementMagnitude * 2)), Color.green, timeTillMove / snakeSpeed);
+                    Debug.DrawLine(transform.position, transformPos + (_snakeDirection * (movementMagnitude * 2)), Color.green, timeTillMove / snakeSpeed);
                 }
 
                 _inputLock = false;
@@ -113,9 +136,11 @@ namespace _02_Scripts
 
         private bool CheckOutOfBounds(Vector3 nextPosition)
         {
-            // TODO: Check if nextPosition is outside of RoomBounds and return if true
-            // Check NextX < -RoomWidth or NextX > RoomWidth
-            // check NextY < -RoomHeight or Next > Roomheight
+            if (nextPosition.x > (_gameController.RoomWidth / 2) || nextPosition.x < -(_gameController.RoomWidth / 2))
+                return true;
+
+            if (nextPosition.y > (_gameController.RoomHeight / 2) || nextPosition.y < -(_gameController.RoomHeight / 2))
+                return true;
 
             return false;
         }
@@ -124,12 +149,26 @@ namespace _02_Scripts
         {
             for (int i = 0; i < _snakeBody.SnakeLength; i++)
             {
-                // TODO: Check if nextPosition of head is equal to the BodyPart and return if true
-                // if (nextPosition.Equals(_snakeBody.BodyParts[i].Position)) return true;
+                if (nextPosition == _snakeBody.BodyParts[i].Position) return true;
             }
+            
             return false;
         }
-        
+
+
+        private int CheckFoodCollision(Vector3 nextPosition)
+        {
+            List<GameObject> listFood = _gameController.ListFood;
+            for (int i = 0; i < listFood.Count; i++)
+            {
+                if (nextPosition == listFood[i].transform.position)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
     }
 
     public class SnakeBody
@@ -146,6 +185,17 @@ namespace _02_Scripts
             this._bodyPartPrefab = bodyPartPrefab;
             this._snakeBodyParent = snakeBodyParent;
 
+            InitBody(startLength);
+        }
+
+        public void ResetBody(int startLength)
+        {
+            ClearBodyParts();
+            InitBody(startLength);
+        }
+
+        private void InitBody(int startLength)
+        {
             for (int i = 0; i < startLength; i++)
             {
                 AddBodyPart();
@@ -157,6 +207,15 @@ namespace _02_Scripts
             Vector3 lastBodyPartPosition = _bodyParts.Count > 0 ? _bodyParts[^1].Position : _snakeBodyParent.position;
             BodyPart newBodyPart = new(_bodyPartPrefab, lastBodyPartPosition);
             _bodyParts.Add(newBodyPart);
+        }
+
+        private void ClearBodyParts()
+        {
+            foreach (BodyPart t in _bodyParts)
+            {
+                t.DestroyBodyPart();
+            }
+            _bodyParts.Clear();
         }
 
         public void MoveBodyParts(Vector3 headPosition)
@@ -181,9 +240,14 @@ namespace _02_Scripts
             get => _bodyObject.transform.position;
             set => _bodyObject.transform.position = value;
         }
-        public BodyPart(GameObject bodyPartPrefab,Vector3 transformPos)
+        public BodyPart(GameObject bodyPartPrefab, Vector3 transformPos)
         {
             this._bodyObject = Object.Instantiate(bodyPartPrefab, transformPos, Quaternion.identity);
+        }
+
+        public void DestroyBodyPart()
+        {
+            Object.Destroy(_bodyObject);
         }
     }
 }
